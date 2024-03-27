7b41a8c32fe95d2a4ac83db866cabd11ece855f0
Tommy Chen <tchen175@ucsc.edu>
2024-03-26 17:01:34 -0700

    a

---
+++ website/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import Header from './Components/Header.jsx'
+import AboutMe from './Components/AboutMe.jsx'
+import ProjectSection from './Components/ProjectSection.jsx'
+
+function App() {
+  
+  return (
+    <>
+      <Header/>
+      <AboutMe/>
+      <ProjectSection/>
+    </>
+  )
+  
+
+}
+
+export default App
